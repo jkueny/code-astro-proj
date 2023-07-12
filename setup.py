8049925df28c1da5280883cbf933b102e3a97e89
Gemma <gemma.gonzaleztora@gmail.com>
2023-07-12 17:02:18 +0200

    include set up file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="FindDouble",
+	version="0.1",
+	description="Find doubles in Gaia by cross-matching with WDS catalog",
+	license="MIT",
+	packages=find_packages(),
+	authors='Gonzalez-Tora, G., Kueny, J., Li, J.'
+)
+	
