0ef1d5c13f614ff1d5751c96ed5a7ff03cc0a93a
Gemma <gemma.gonzaleztora@gmail.com>
2023-07-13 16:02:06 +0200

    included the documentation, setup.py and __init__.py and a .py file with a trial function for the documentation

---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. SearchDoubles documentation master file, created by
+   sphinx-quickstart on Wed Jul 12 18:14:21 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to SearchDoubles's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   search_doubles_v1.rst
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ search_doubles/search_doubles_v1.py
@@ -0,0 +1,60 @@
+#Import the necessary modules
+from astropy import units as u
+from astropy.coordinates import SkyCoord, Angle
+#from astropy.wcs import WCS
+import warnings
+import numpy as np
+warnings.filterwarnings('ignore')
+from astroquery.vizier import Vizier
+#from IPython.display import Image
+#from IPython.core.display import HTML
+import pandas as pd
+from astroquery.simbad import Simbad
+
+def query_the_gaia(objloc,conerad,catalognamelist=["I/350/gaiaedr3","B/wds"],RUWE=True,maghigh=3,maglow=10):
+    """
+    This function will query the specified catalogs using Astropy Vizier.
+
+    Args:
+        catalognamelist (list): List of catalog name strings as shown on Vizier. 
+        Ex. "I/350/gaiaedr3"
+        objra (float): RA coord in degrees
+        objdec (float): Dec coord in degrees
+        conerad (float): Cone radius in degrees
+
+    Returns:
+        _type_: _description_
+    """    
+    if len(objloc) == 2:
+         obj_coord=SkyCoord(objloc[0],objloc[1],unit=(u.degree, u.degree), frame='icrs')
+    else:
+        obj_coord=SkyCoord(objloc,unit=(u.hourangle, u.degree), frame='icrs')
+    result = Vizier.query_region(obj_coord,
+                            radius=u.Quantity(conerad,u.deg),
+                             catalog=catalognamelist )
+    
+    
+    if RUWE:
+        result=result[0][result[0]['RUWE']<1.2]
+
+    #filtering more by G magnitude
+    result=result[result['Gmag']>maghigh] 
+    result=result[result['Gmag']<maglow]
+        
+    gaia_id_list=result['Source']
+
+    # TODO add column headers for name, RA, DEC, RUWE, mag
+    singles=[]
+    for i in gaia_id_list:
+        id= "Gaia DR3"+str(i)
+        info=Simbad.query_objectids(id)
+        strinfo=str(info)
+        if 'wds' in strinfo:
+            gaia_id_list.remove(i)
+        else:
+            singles.append(info[0])
+
+    return singles
+
+#temp_table = query_the_gaia(objloc=scoords,conerad=srad,catalognamelist=["I/355/gaiadr3"])
+#print(temp_table)
