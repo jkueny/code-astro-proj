b4304796f74fe7ee3719cd5fb101435e428749fb
Jay Kueny <jkueny@email.arizona.edu>
2023-07-14 00:50:02 -0500

    added function to determine coordinate units

--- non_binary_finder/find_single_stars.py
+++ non_binary_finder/find_single_stars.py
@@ -12,27 +12,63 @@ import matplotlib.pyplot as plt
 from IPython.display import Image
 from IPython.core.display import HTML
 import pandas as pd
+import re
+
+def determine_coord_units(incoords):
+    ok_chars = r'[^0-9\s+-.]'  # re filter any non-numericals; keeping spaces, decimal pts, +/-
+    found_chars_not_ok = re.findall(ok_chars, incoords)
+    print(found_chars_not_ok)
+    if (type(incoords) == list) or (type(incoords) == tuple):
+        if len(incoords) > 1:
+            raise TypeError("Pls remove unecessary brackets; only separate RA/Dec with spaces.")
+    if type(incoords) != str:
+        raise TypeError("Please input RA/Dec pair as a string.")
+    if ":" in  incoords:
+        raise ValueError("Please separate HMS values by spaces only.")
+    if len(found_chars_not_ok) > 0:
+        raise ValueError("Invalid character(s)! Please separate by spaces only and no brackets.")
+    deconstruct_coords_string = incoords.split()
+    if len(deconstruct_coords_string) > 2:
+        print('Recognized input coordinate in HMS format.')
+        coord_units = (u.degree, u.degree)
+    else:
+        print('Recognized input coordinate in degree format.')
+        coord_units = (u.hourangle, u.degree)
+    return coord_units
+
 
 def query_the_gaia(objloc,conerad,catalognamelist=["I/350/gaiaedr3","B/wds"],RUWE=True,maghigh=3,maglow=10):
     """
+
     This function will query the input list of catalogs using the Astropy Vizier API.
 
     Args:
-        catalognamelist (list): List of catalog name strings as shown on Vizier. 
-        Ex. "I/350/gaiaedr3"
-        objloc (string): RA/Dec coordinate pair to pass into Vizier.query_region().
+        - objloc (string): RA/Dec coordinate pair to pass into Vizier.query_region().
         Ex. "11 02 24.8763629208 -77 33 35.667131796"
-        conerad (float): Cone radius in degrees.
+
+        - conerad (float): Cone radius in degrees.
+
+        - catalognamelist (list): List of catalog name strings as shown on Vizier. [optional]
+        Ex. "I/350/gaiaedr3"
+
+        - RUWE (bool): Toggle RUWE filtering metric (refer to README for theory) [optional]
+
+        - maghigh (float): Brightness limit for queried objects. [optional]
+
+        - maglow (float): Faintness limit for queried objects. [optional]
 
     Returns:
-        Pandas DataFrame: CSV saved to disk in the directory where this script is located.
+        pd.DataFrame: CSV saved to disk in the directory where this script is located.
+
     """ 
     ## change row limit to none; else default to 50 
-    Vizier.ROW_LIMIT = -1   
-    if len(objloc) == 2:
-         obj_coord=SkyCoord(objloc[0],objloc[1],unit=(u.degree, u.degree), frame='icrs')
-    else:
-        obj_coord=SkyCoord(objloc,unit=(u.hourangle, u.degree), frame='icrs')
+    Vizier.ROW_LIMIT = -1
+    input_coord_units = determine_coord_units(objloc)
+    obj_coord = SkyCoord(objloc,unit=(input_coord_units), frame='icrs')
+    # if len(objloc) == 2:
+    #      obj_coord=SkyCoord(objloc[0],objloc[1],unit=input_coord_units), frame='icrs')
+    # else:
+    #     obj_coord=SkyCoord(objloc,unit=input_coord_units, frame='icrs')
     
     #making sure cone radius value is proper
 
@@ -83,6 +119,9 @@ def query_the_gaia(objloc,conerad,catalognamelist=["I/350/gaiaedr3","B/wds"],RUW
     return sorted_df
 
 
-# print(query_the_gaia(objloc="11 02 24.8763629208 -77 33 35.667131796",
+# print(query_the_gaia(objloc=("11 02 24.8763629208 -77 33 35.667131796"),
+#                conerad=0.5,
+#                ))
+# print(query_the_gaia(objloc=("165.520318183 -77.5599075361"),
 #                conerad=0.5,
 #                ))
